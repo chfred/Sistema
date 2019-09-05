e0ca35f8de281b593128e214948f0b739ede288f
Fredych <fredy.ch@outlook.com>
2019-09-04 22:46:26 -0600

    Estructura báse del proyecto

---
+++ sistema/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# --------------------------------------------------------------------------------
+DEBUG = False
+
+ALLOWED_HOSTS = ['*'] 
+
+# Database
+# --------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
